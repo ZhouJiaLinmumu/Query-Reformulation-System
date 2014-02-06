1c216dd195d7a8d65e16c3ffea23c3291ae0eb0a
=Abhinav Bajaj <bajajabhi1@gmail.com>
2014-02-05 23:52:00 -0500

    basic framework

--- main.py
+++ main.py
@@ -3,15 +3,17 @@ import urllib
 import urllib2
 import base64
 import json
+import sys
 
 def main():
-    query = "gates"
     query = raw_input('Please enter the query you want to search for : ')
-    preck = raw_input('Please enter the precision you want to search with (0-1) : ')
-    bing_search(query)
+    query = query.replace(" ",'+')
+    targetPrec = raw_input('Please enter the precision(@10) you want to search with (0-1) : ')
+    bing_search(query, targetPrec)
 
-def bing_search(query):
-    bingUrl = 'https://api.datamarket.azure.com/Bing/Search/Web?Query=%27gates%27&$top=10&$format=json'
+def bing_search(query,targetPrec):
+    bingUrl = 'https://api.datamarket.azure.com/Bing/Search/Web?Query=%27' + query + '%27&$top=10&$format=json'
+    print bingUrl
     #Provide your account key here
     accountKey = 'JsV9AIVwzY0l654YiaIXAppMcpvpm7lvkcYdmzJrNcs'
 
@@ -21,25 +23,38 @@ def bing_search(query):
     response = urllib2.urlopen(req)
     content = response.read()
     print content
-    #content contains the xml/json response from Bing. 
+    #content contains the json response from Bing. 
     json_result = json.loads(content)
     result_list = json_result['d']['results']
-    print getRelJudmnt(result_list)
-    return result_list
+    print getRelevantFB(query, result_list,targetPrec)
 
-def getRelJudmnt(result_list):
-    count = 0;
+def getRelevantFB(query, result_list, targetPrec):
+    userPrec = 0.0;
+    relevant = []
     for result in result_list:
-      desc = result[u'Description'].encode("iso-8859-15", "replace")
-      title = result[u'Title'].encode("iso-8859-15", "replace")
-      url = result[u'Url'].encode("iso-8859-15", "replace")
-      print '\nTitle: ' + title + '\n' + 'Url: ' + url + '\n' + 'Desc: ' + desc
-      isRel = raw_input('Is this link relevant to your search or not (y or n)?: ')
-      if isRel == 'y':
-          count = count+1
-          
-    return count
+        desc = result[u'Description'].encode("iso-8859-15", "replace")
+        title = result[u'Title'].encode("iso-8859-15", "replace")
+        url = result[u'Url'].encode("iso-8859-15", "replace")
+        print '\nTitle: ' + title + '\n' + 'Url: ' + url + '\n' + 'Desc: ' + desc
+        isRel = raw_input('Is this link relevant to your search or not (y or n)?: ')
+        if isRel == 'y' or isRel=='Y':
+            userPrec = userPrec+1
 
+    print userPrec
+    userPrec = userPrec/10
+    print userPrec
+    # If targetPrecision is achieved
+    if userPrec == 0 or userPrec >= targetPrec:
+        sys.exit()
+    else:
+        keyWordEngine(query,targetPrec)
+
+
+def keyWordEngine(query,targetPrec):
+    print 'Finding new key words'
+    print 'Determining new key words'
+    print 'Updated the query' + query
+    bing_search(query,targetPrec)
 
 if __name__ == "__main__":
     main()
