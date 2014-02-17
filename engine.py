3c06fa77a26c62dc7a6610f1b658fc992dad322b
rafica <rafica.ar@gmail.com>
2014-02-16 20:27:19 -0500

    ordered the modified query

--- engine.py
+++ engine.py
@@ -1,7 +1,7 @@
 import string
 import math
 import re
-
+import itertools
 
 stopwords = ['a', 'able', 'about', 'across', 'after', 'all', 'almost', 'also', 'am', 'among', 'an', 'and', 'any', 'are', 'as',
              'at', 'be', 'because', 'been', 'but', 'by', 'can', 'cannot', 'could', 'dear', 'did', 'do', 'does', 'either', 'else',
@@ -15,21 +15,19 @@ stopwords = ['a', 'able', 'about', 'across', 'after', 'all', 'almost', 'also', '
 
 def keyWordEngine(query,targetPrec,relevant,nonrel):
     query = query.replace('+',' ')
-    print "query is " + query
+
     # finding N for calculating IDF
     N_Rel = len(relevant)
     N_Nonrel = len(nonrel)
 
     #finding TF
     tfRel,titleRel = findTF(relevant)
-    print 'Rel title list'
-    print titleRel
+
     
     tfNonRel,titleNonRel = findTF(nonrel)
     
     tfQuery = findQueryTF(query)
-    print 'Non Rel title list'
-    print titleNonRel
+
     
     #finding IDF
     idfRel = findIDF(tfRel, N_Rel)
@@ -39,26 +37,105 @@ def keyWordEngine(query,targetPrec,relevant,nonrel):
     weightsRel = {}
     weightsRel = findWeights(tfRel, idfRel, titleRel, N_Rel)
 
-    print "weights relevant"
-    print weightsRel
+
     
     #finding Nonrelevant weights
     weightsNonRel = {}
     weightsNonRel = findWeights(tfNonRel, idfNonRel, titleNonRel, N_Nonrel)
 
-    print "weights non relevant "
-    print weightsNonRel
+
 
     #implementing Rochio to find the new query
     (first,second) = findWords(weightsRel, weightsNonRel, tfQuery)
 
+    finalList = []
     
     print 'New words added to query are - ' + first + ' ' + second
     #original query modified
+    query = query.split()
     if second=="":
-        return query+' '+first
+        finalList.extend(query)
+        finalList.append(first)
     else:
-        return query + ' '+ first + ' '+ second
+        finalList.extend(query)
+        finalList.append(first)
+        finalList.append(second)
+
+    #find the best order of the words in the query        
+    finalOrderedList = findCombination(finalList, relevant)
+
+    modifiedQuery = []
+    for wordList in finalOrderedList:
+        for word in wordList:
+            modifiedQuery.append(word)
+            
+    #clusters are just appended for now
+    return " ".join(modifiedQuery)
+
+
+def searchResults(phrase, docs):
+    weight = 0
+    for doc in docs:
+        content = doc['Title'] + ' ' + doc['Description']
+        #Converting to lowercase
+        content = content.lower()
+        #Removing punctuation
+        content = content.translate(string.maketrans("",""), string.punctuation)
+        matches = re.findall(re.escape(phrase[0])+'\s'+re.escape(phrase[1]), content)
+        weight = weight + len(matches)
+    return weight
+            
+    
+
+def findCombination(queryList,docRel):
+    maxPair = 0
+    pairWeight = {}
+    correctQueryList = []
+    for pair in itertools.permutations(queryList, 2):
+        pairWeight[pair] = searchResults(pair,docRel)
+    sortedPairs = sorted(pairWeight.items(), key=lambda x:x[1], reverse = True)
+
+    print "sorted pair list"
+    print sortedPairs
+
+    N = len(queryList)
+    added = 0
+    for pair in sortedPairs:
+        print "for each pair"
+        print pair[0]
+        correctQueryList,added = addPair(0, correctQueryList, pair[0], added)
+        if added == N:
+            break
+        
+    print "corrected QueryList clusters"
+    print correctQueryList
+    return correctQueryList
+
+def addPair(index ,QueryList, pair, added):
+    
+    if len(QueryList)<=index:
+        QueryList.append([])
+        QueryList[index].append(pair[0])
+        QueryList[index].append(pair[1])
+        added = added + 2
+    else:
+        n = len(QueryList[index])
+        if pair[0] in QueryList[index] and pair[1] in QueryList[index]:
+            return QueryList,added
+        if pair[0]==QueryList[index][n-1]:
+            QueryList[index].append(pair[1])
+            added = added + 1
+        elif pair[1]==QueryList[index][0]:
+            QueryList[index].insert(0,pair[0])
+            added = added + 1
+        elif pair[0] not in QueryList[index] and pair[1] not in QueryList[index]:
+            QueryList,added = addPair(index+1, QueryList, pair, added)
+        #else if only one word of the pair is in the middle of the query then ignore that pair and leave the other word </3
+        #else if first word in pair equals first word in querylist, or vice versa, ignore
+            
+    print QueryList
+    return QueryList,added
+
 
 
 def findWords(RelDoc, NonrelDoc, query):
@@ -71,8 +148,7 @@ def findWords(RelDoc, NonrelDoc, query):
     second = ''
     finalWeight[first]=0
     finalWeight[second]=0
-    print "Query is "
-    print query
+
     for word in RelDoc:
         finalWeight[word] = beta * RelDoc[word]
     for word in query:
@@ -88,11 +164,8 @@ def findWords(RelDoc, NonrelDoc, query):
             finalWeight[word] = gamma * NonrelDoc[word]
 
 
-    print "final weights"
-
-
     sortWeights = sorted(finalWeight.items(), key=lambda x:x[1], reverse = True)
-    print sortWeights
+
 
     #Add only if the new word is not in query
     i = 0
@@ -139,8 +212,7 @@ def findWeights(tfDict, idfDict, titleDict, N):
             tfTotal = tfTotal + tfDict[word][doc]
         weight[word] = tfTotal * idf
         if word in titleDict: # give more weightage to title words
-            weight[word] = weight[word]*(1 + (float(titleDict[word])/float(N))*0.2)
-        
+            weight[word] = weight[word]*(1 + (float(titleDict[word])/float(N))*0.2)        
     return weight
 
 
